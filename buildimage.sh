94755502372adb31905995cc30f5eeef52d592b3
root <root@node1.example.com>
2023-11-25 12:10:29 -0800

    updated file

--- buildimage.sh
+++ buildimage.sh
@@ -12,5 +12,11 @@ echo "Building Image: ${imagename}"
 
 docker build --no-cache -t ${imagename} .
 
+h the image to the IBS registry
+export DOCKER_USERNAME='0403456'
+export DOCKER_PASSWORD='Kubernetesjenk@1234'
 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push ${imagename}
+docker rmi ${imagename}
 
